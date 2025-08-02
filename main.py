f6629ae55d03bcf3b114d85e0d927e6499ed6ca9
amjad awad <amjadawad129@gmail.com>
2025-08-02 23:12:54 +0300

    test the functinality in main file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 from config import PDFConfig
 from extractor import PdfExtractor
+from summaries import OllamaSummaries
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pdf_config = PDFConfig(filename = "Attention-Is-All-You-Need.pdf")
 
@@ -7,6 +12,23 @@ pdf_extractor = PdfExtractor(pdf_config)
 
 texts, images, tables = pdf_extractor.extract(pdf_extractor.chunks)
 
-print("texts", texts)
-print("images", images)
-print("tables", tables)
+print("Extraction complete")
+
+ollama_summaries = OllamaSummaries()
+
+summaries_texts, summaries_images, summaries_tables = ollama_summaries.summaries(texts, images, tables)
+
+if not os.path.isdir("summaries_return"):
+    os.mkdir("summaries_return")
+
+with open("summaries_return/summaries_text.txt", "w", encoding='utf-8') as text_file:
+    text_file.write("\n----------------------------------------------\n".join(summaries_texts))
+
+with open("summaries_return/summaries_images.txt", "w", encoding='utf-8') as images_file:
+    images_file.write("\n----------------------------------------------\n".join(summaries_images))
+
+with open("summaries_return/summaries_tables.txt", "w", encoding='utf-8') as tables_file:
+    tables_file.write("\n----------------------------------------------\n".join(summaries_tables))
+
+
+print("Summaries complete")
